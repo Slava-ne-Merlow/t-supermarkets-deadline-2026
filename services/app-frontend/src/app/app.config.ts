25928e2948a6f40bfd834f3c0bc42bcac8b3e734
vaceslavkusenko <SlavaKushch@yandex.ru>
2026-04-27 13:49:06 +0300

    fix: provide taiga ui root config

--- services/app-frontend/src/app/app.config.ts
+++ services/app-frontend/src/app/app.config.ts
@@ -1,7 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideTaiga } from '@taiga-ui/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true })
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideTaiga({
+      apis: 'stable',
+      fontScaling: false,
+      scrollbars: 'native'
+    })
   ]
 };
